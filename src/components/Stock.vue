b61a5c1b3a45838fe2b7bf7cb3e8d74792114be7
Vincent <c.tnecniv@gmail.com>
2017-01-08 00:48:54 -0800

    pulled out stock component

---
+++ src/components/Stock.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="result">
+    <div class="title">
+      <div class="change up">
+        +1.3(1.11%)
+      </div>
+      <div class="symbol">
+        Apple
+        <span class="bold">
+          (AAPL)
+        </span>
+      </div>
+      <div class="subtext">
+        <span class="label">Open</span> $116.78
+      </div>
+      <div class="subtext">
+        <span class="label">Volume</span> 31,751,900
+      </div>
+      <div class="subtext">
+        <span class="label">52 Week High</span> $119.86
+      </div>
+      <div class="subtext">
+        <span class="label">52 Week Low</span> $89.47
+      </div>
+      <div class="subtext">
+        <span class="label">Industry</span> Consumer Electronics
+      </div>
+    </div>
+    <div class="details">
+      <div class="value-item">
+        <div class="label">
+          bid
+        </div>
+        <div class="value">
+          <span class="dollar-sign">$</span>117<span class="cents">.79</span>
+        </div>
+      </div>
+      <div class="value-item">
+        <div class="label">
+          ask
+        </div>
+        <div class="value">
+          <span class="dollar-sign">$</span>117<span class="cents">.84</span>
+        </div>
+      </div>
+    </div>
+    <div class="actions">
+      <button type="button" class="outline">sell</button>
+      <button type="button">buy</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data', 'maxSell'],
+  data () {
+    return {}
+  },
+  methods () {
+
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped="true">
+@import "../assets/css/vendor/bourbon";
+@import "../assets/css/colors";
+
+.result {
+  background-color: #4d505a;
+  border-radius: 3px;
+  margin-top: 20px;
+  overflow: hidden;
+  box-shadow: 0 0 15px shade(#4c515b, 25%);
+  .title {
+    background-color: #5b5f69;
+    padding: 20px 20px;
+    font-weight: lighter;
+    position: relative;
+    .change {
+      position: absolute;
+      right: 20px;
+      top: 20px;
+      font-size: 25px;
+      &.up {
+        color: $green;
+      }
+    }
+    .symbol {
+      font-size: 25px;
+      letter-spacing: 1px;
+      margin-bottom: 15px;
+    }
+    .subtext {
+      font-size: 14px;
+      line-height: 1.6em;
+      .label {
+        color: tint($dark, 45%);
+        margin-right: 2px;
+      }
+    }
+  }
+  .details {
+    display: flex;
+    padding: 30px 0;
+    .value-item {
+      width: 50%;
+      border-right: 1px solid tint($prime, 15%);
+      .value {
+        font-size: 40px;
+      }
+    }
+  }
+  .actions {
+    display: flex;
+    margin: 6px;
+    button {
+      width: 50%;
+      margin: 6px;
+    }
+  }
+}
+</style>
--- src/components/Trade.vue
+++ src/components/Trade.vue
@@ -97,73 +97,39 @@
         </div>
       </div>
       <div class="symbol-search-wrap right">
-        <input type="text" placeholder="Search for a symbol..." tabindex="1">
-        <div class="result">
-          <div class="title">
-            <div class="change up">
-              +1.3(1.11%)
-            </div>
-            <div class="symbol">
-              Apple
-              <span class="bold">
-                (AAPL)
-              </span>
-            </div>
-            <div class="subtext">
-              <span class="label">Open</span> $116.78
-            </div>
-            <div class="subtext">
-              <span class="label">Volume</span> 31,751,900
-            </div>
-            <div class="subtext">
-              <span class="label">52 Week High</span> $119.86
-            </div>
-            <div class="subtext">
-              <span class="label">52 Week Low</span> $89.47
-            </div>
-            <div class="subtext">
-              <span class="label">Industry</span> Consumer Electronics
-            </div>
-          </div>
-          <div class="details">
-            <div class="value-item">
-              <div class="label">
-                bid
-              </div>
-              <div class="value">
-                <span class="dollar-sign">$</span>117<span class="cents">.79</span>
-              </div>
-            </div>
-            <div class="value-item">
-              <div class="label">
-                ask
-              </div>
-              <div class="value">
-                <span class="dollar-sign">$</span>117<span class="cents">.84</span>
-              </div>
-            </div>
-          </div>
-          <div class="actions">
-            <button type="button" class="outline">sell</button>
-            <button type="button">buy</button>
-          </div>
+        <div class="input-wrap"
+             :class="inputClass()">
+          <input type="text"
+                 placeholder="Search for a symbol..." tabindex="1">
+          <span class="error"></span>
+          <img src="https://dsgcewkenvygd.cloudfront.net/assets/balls.svg"/>
         </div>
+
+        <stock v-if="currentStock"></stock>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Stock from './Stock'
+
 export default {
   data () {
     return {
       balance: null,
       portfolio: [],
-      initialLoading: true
+      initialLoading: true,
+      currentStock: null
     }
   },
-  methods () {
-
+  components: {
+    Stock
+  },
+  methods: {
+    inputClass () {
+      return 'loading'
+    }
   },
   created () {
     this.balance = this.getBalance()
@@ -236,50 +202,31 @@ export default {
       }
     }
   }
-  .value-item {
-    text-align: center;
-    border-right: 1px solid tint($prime, 15%);
-    padding: 20px 0;
-    &:last-child {
-      border-right: none;
-    }
-    .label {
-      text-transform: uppercase;
-      font-size: 13px;
-      opacity: 0.5;
-      letter-spacing: 1px;
-    }
-    .value {
-      font-size: 30px;
-      letter-spacing: 1px;
-      margin-top: 20px;
-      .dollar-sign {
-        font-size: 14px;
-        position: relative;
-        top: -1px;
-        margin-right: 2px;
-      }
-      .cents {
-        font-size: 14px;
-      }
-    }
-  }
   .symbol-search-wrap {
-    & > input {
-      box-sizing: border-box;
-      width: 100%;
-      background-color: transparent;
-      border: 1px solid tint($prime, 10%);
-      border-radius: 999px;
-      padding: 12px 19px;
-      color: #FFFFFA;
-      &:hover {
-        cursor: pointer;
-        border: 1px solid tint($prime, 15%);
+    .input-wrap {
+      position: relative;
+      & > input {
+        box-sizing: border-box;
+        width: 100%;
+        background-color: transparent;
+        border: 1px solid tint($prime, 10%);
+        border-radius: 999px;
+        padding: 12px 19px;
+        color: #FFFFFA;
+        &:hover {
+          cursor: pointer;
+          border: 1px solid tint($prime, 15%);
+        }
+        &:focus {
+          border: 1px solid tint($prime, 15%);
+          background-color: tint($prime, 5%)
+        }
       }
-      &:focus {
-        border: 1px solid tint($prime, 15%);
-        background-color: tint($prime, 5%)
+      img {
+        position: absolute;
+        right: 5px;
+        top: 0px;
+        height: 44px;
       }
     }
   }
@@ -317,60 +264,6 @@ export default {
           cursor: pointer;
           background-color: tint(#4c515b, 5%);
         }
-      }
-    }
-  }
-  .result {
-    background-color: #4d505a;
-    border-radius: 3px;
-    margin-top: 20px;
-    overflow: hidden;
-    box-shadow: 0 0 15px shade(#4c515b, 25%);
-    .title {
-      background-color: #5b5f69;
-      padding: 20px 20px;
-      font-weight: lighter;
-      position: relative;
-      .change {
-        position: absolute;
-        right: 20px;
-        top: 20px;
-        font-size: 25px;
-        &.up {
-          color: $green;
-        }
-      }
-      .symbol {
-        font-size: 25px;
-        letter-spacing: 1px;
-        margin-bottom: 15px;
-      }
-      .subtext {
-        font-size: 14px;
-        line-height: 1.6em;
-        .label {
-          color: tint($dark, 45%);
-          margin-right: 2px;
-        }
-      }
-    }
-    .details {
-      display: flex;
-      padding: 30px 0;
-      .value-item {
-        width: 50%;
-        border-right: 1px solid tint($prime, 15%);
-        .value {
-          font-size: 40px;
-        }
-      }
-    }
-    .actions {
-      display: flex;
-      margin: 6px;
-      button {
-        width: 50%;
-        margin: 6px;
       }
     }
   }
